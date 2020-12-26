4b3c5c7e1d060b6bd8232f720901381dc1fa9141
Tung Luu <luukhanhtung2002@gmail.com>
2020-12-26 11:13:20 +0700

    Create main function for stack implementation

--- Algorithms Part I/stacks_and_queues/FixedCapacityStack.java
+++ Algorithms Part I/stacks_and_queues/FixedCapacityStack.java
@@ -1,4 +1,5 @@
 package stacks_and_queues;
+import java.util.Scanner;
 
 public class FixedCapacityStack {
     private String[] stack;
@@ -12,6 +13,10 @@ public class FixedCapacityStack {
         return numItem == 0;
     }
 
+    public int size() {
+        return numItem;
+    }
+
     public void push(String item) {
         stack[numItem] = item;
         numItem++;
@@ -22,5 +27,47 @@ public class FixedCapacityStack {
         String item = stack[numItem];
         stack[numItem] = null; // Free the memory - avoid loitering
         return item;
+    }
+
+    public void printAllItems() {
+        for (String item : stack) {
+            if (item != null) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int capactiy = input.nextInt();
+        FixedCapacityStack stack = new FixedCapacityStack(capactiy);
+
+        input.nextLine();
+        while(true) {
+            String item = input.nextLine();
+            if (item.isEmpty()) {
+                break;
+            } 
+
+            if (!item.equals("-")) {
+                if (stack.size() < capactiy) {
+                    stack.push(item);
+                }
+                else {
+                    System.out.println("Stack is full");
+                }
+            }
+            else {
+                if (!stack.isEmpty()) {
+                    System.out.println(stack.pop());
+                }
+                else {
+                    System.out.println("Stack is empty");
+                }
+            }
+        }
+        input.close();
+        System.out.println("Items left on stack: ");
+        stack.printAllItems();
     }
 }
--- Algorithms Part I/stacks_and_queues/LinkedStack.java
+++ Algorithms Part I/stacks_and_queues/LinkedStack.java
@@ -1,13 +1,15 @@
 package stacks_and_queues;
+import java.util.Scanner;
 
-public class LinkedStack {
+public class LinkedStack<Data> {
     private Node head = null;
+    private int size = 0;
 
     private class Node {
-        String data;
+        Data data;
         Node next;
 
-        public Node(String data) {
+        public Node(Data data) {
             this.data = data;
             this.next = null;
         }
@@ -16,16 +18,47 @@ public class LinkedStack {
     public boolean isEmpty() {
         return head == null;
     }
+
+    public int size() {
+        return size;
+    }
     
-    public void push(String data) {
+    public void push(Data data) {
         Node newNode = new Node(data);
         newNode.next = head;
         head = newNode;
+        size++;
     }
 
-    public String pop() {
-        String data = head.data;
+    public Data pop() {
+        Data data = head.data;
         head = head.next;
+        size--;
         return data;
+    }
+
+    public static void main(String[] args) {
+        LinkedStack<String> stack = new LinkedStack<String>();
+        Scanner input = new Scanner(System.in);
+        while(true) {
+            String data = input.nextLine();
+            if (data.isEmpty()) {
+                break;
+            } 
+
+            if (!data.equals("-")) {
+                stack.push(data);
+            }
+            else {
+                if (!stack.isEmpty()) {
+                    System.out.println(stack.pop());
+                }
+                else {
+                    System.out.println("Stack is empty");
+                }
+            }
+        }
+        input.close();
+        System.out.println(stack.size());
     }
 }
