6cf6aa15914093e6598672ac9751ff9cced19d6e
Tung Luu <luukhanhtung2002@gmail.com>
2020-12-29 10:27:28 +0700

    Test MaxPQBinaryHeap:

--- Algorithms Part I/priority_queue/MaxPQBinaryHeap.java
+++ Algorithms Part I/priority_queue/MaxPQBinaryHeap.java
@@ -1,11 +1,13 @@
 package priority_queue;
 
+import java.util.Scanner;
+
 public class MaxPQBinaryHeap<Key extends Comparable<Key>> {
-    private Key[] priorityQueues;
+    private Key[] priorityQueue;
     private int numNode;
 
     public MaxPQBinaryHeap(int capactiy) {
-        priorityQueues = (Key[]) new Comparable[capactiy + 1]; // +1 because we will not use index 0
+        priorityQueue = (Key[]) new Comparable[capactiy + 1]; // +1 because we will not use index 0
     }
 
     public boolean isEmpty() {
@@ -14,15 +16,15 @@ public class MaxPQBinaryHeap<Key extends Comparable<Key>> {
 
     public void insert(Key key) {
         numNode++;
-        priorityQueues[numNode] = key;
+        priorityQueue[numNode] = key;
         rise(numNode);
     }
 
     public Key deleteMax() {
-        Key max = priorityQueues[1]; // The root
+        Key max = priorityQueue[1]; // The root
         swap(1, numNode);
         // Delete the max node
-        priorityQueues[numNode] = null;
+        priorityQueue[numNode] = null;
         numNode--;
         // Sink the top node to the right position
         sink(1);
@@ -53,12 +55,48 @@ public class MaxPQBinaryHeap<Key extends Comparable<Key>> {
     }
 
     private boolean less(int i, int j) {
-        return priorityQueues[i].compareTo(priorityQueues[j]) < 0;
+        return priorityQueue[i].compareTo(priorityQueue[j]) < 0;
     }
 
     private void swap(int i, int j) {
-        Key temp = priorityQueues[i];
-        priorityQueues[i] = priorityQueues[j];
-        priorityQueues[j] = temp;
+        Key temp = priorityQueue[i];
+        priorityQueue[i] = priorityQueue[j];
+        priorityQueue[j] = temp;
+    }
+
+    public void printAllItems() {
+        for (Key key : priorityQueue) {
+            if (key != null) {
+                System.out.println(key);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int capactiy = input.nextInt();
+        MaxPQBinaryHeap<String> priorityQueue = new MaxPQBinaryHeap<String>(capactiy);
+
+        input.nextLine();
+        while(true) {
+            String item = input.nextLine();
+            if (item.isEmpty()) {
+                break;
+            } 
+
+            if (!item.equals("-")) {
+                priorityQueue.insert(item);
+            }
+            else {
+                if (!priorityQueue.isEmpty()) {
+                    System.out.println(priorityQueue.deleteMax());
+                }
+                else {
+                    System.out.println("Queue is empty");
+                }
+            }
+        }
+        input.close();
+        System.out.println("Items left on Queue: ");
+        priorityQueue.printAllItems();
     }
 }
